38bbb3f2e9fa8f1d5e8d69837f1a446c37c71141
Robert S <rseifert.phone@gmail.com>
2014-07-25 20:23:24 -0400

    Added the start to grief prevention support

--- src/main/java/com/builtbroken/region/blacklist/gp/GriefSupport.java
+++ src/main/java/com/builtbroken/region/blacklist/gp/GriefSupport.java
@@ -1,6 +1,12 @@
 package com.builtbroken.region.blacklist.gp;
 
+import me.ryanhamshire.GriefPrevention.Claim;
+import me.ryanhamshire.GriefPrevention.GriefPrevention;
+import me.ryanhamshire.GriefPrevention.PlayerData;
+import me.ryanhamshire.GriefPrevention.TextMode;
+
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 import com.builtbroken.region.blacklist.PluginRegionBlacklist;
@@ -24,5 +30,14 @@ public class GriefSupport extends PluginSupport
 	{
 		// TODO Auto-generated method stub
 
+	}
+	
+	@Override
+	public boolean canBuild(Player player, Block block)
+	{
+		String noBuildReason = GriefPrevention.instance.allowBuild(player, block.getLocation());
+		if (noBuildReason == null)
+			return true;
+		return false;
 	}
 }
